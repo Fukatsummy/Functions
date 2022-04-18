6cae3e85f32be7bc0b15e26d4db569250b59985f
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-04-18 21:56:18 +0300

    recursion

--- Recursion/main.cpp
+++ Recursion/main.cpp
@@ -1,6 +1,34 @@
 #include<iostream>
 using namespace std;
 
+void elevator(int floor);
+int factorial(int n);
+double power(double a, int n);
+//#define ELEVATOR
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	/*cout << "Hello World!";
+	main();*/
+
+#ifdef ELEVATOR
+	int n;
+	cout << "Введите номер этажа: "; cin >> n;
+	elevator(n);
+#endif // Elevator
+	/*int n;
+	cout << "Введите число для вычисления Факториала: "; cin >> n;
+	cout << factorial(n) << endl;*/
+
+	int a;
+	int n;
+	cout << "Введите основание степени: "; cin >> a;
+	cout << "Введите показатель степени: "; cin >>n ;
+	cout << power(a, n) << endl;
+	main();
+
+}
 void elevator(int floor)
 {
 	if (floor == 0)
@@ -9,17 +37,22 @@ void elevator(int floor)
 		return;
 	}
 	cout << "Вы на " << floor << "этаже\n";
-	elevator(floor-1);
+	elevator(floor - 1);
 	cout << "Вы на " << floor << "этаже\n";
 }
-
-void main()
+int factorial(int n)//факториал
 {
-	setlocale(LC_ALL, "");
-	/*cout << "Hello World!";
-	main();*/
-
-	int n;
-	cout << "Введите номер этажа: "; cin >> n;
-	elevator(n);
+	/*if (n < 0)return 0;
+	if (n == 0)return 1;
+	return n*factorial(n - 1);*/
+	
+	return n<0?0:n == 0 ? 1 : n * factorial(n - 1);
+}
+double power(double a, int n) //степень
+{
+	/*if (n == 0)return 1;
+	else if(n>0) return a * power(a, n - 1);
+	else if (n < 0)return 1 / a * power(a, n + 1);*/
+	//return n == 0 ? 1 : n > 0 ? a * power(a, n - 1) : 1 / a * power(a, n + 1);//второй вариант решения
+	return n == 0 ? 1 : n > 0 ? a * power(a, n - 1) : 1 / power(a, -n);//третий вариант
 }
