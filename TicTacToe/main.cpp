542989458578c7610932e94829a6048c21db89b8
Fukatsummy <101829056+Fukatsummy@users.noreply.github.com>
2022-04-22 19:57:24 +0300

    TicTacToe DONE

--- TicTacToe/main.cpp
+++ TicTacToe/main.cpp
@@ -5,6 +5,7 @@ using namespace std;
 #define VERTICAL_SHIFT  "\n\n\n\n\n"
 #define HORIZONTAL_SHIFT  "\t\t\t\t"
 
+void InitField(char field[], const int n, char player);
 void PrintField(char field[], const int n, char player);
 void Move(char field[], const int n, char player);
 void Check(char field[], const int n, char player);
@@ -16,8 +17,20 @@ void main()
 	const int n = 9;
 	char field[n] = {};
 	//for (int i = 0; i < n; i++)field[i] = i + 49;//49 - ASCII-код символа 1
-	PrintField(field, n, '0');
+	//PrintField(field, n, '0');
+	do
+	{
+		InitField(field, n,'0');
+		cout << "Сыграем ещё? Да - anykey, Нет - Escape" << endl;
+	} while (_getch() != 27);
 }
+
+void InitField(char field[], const int n, char player)
+{
+	for (int i = 0; i < n; i++)field[i] = 0;
+	PrintField(field, n, player);
+}
+
 void PrintField(char field[], const int n, char player)
 {
 	system("CLS");//функция system() вызывает любую команду командной строки
@@ -72,6 +85,40 @@ void Check(char field[], const int n, char player)
 		cout << player << " Победа!\n";
 		return;
 	}
+	bool draw = true; //изначально предпологаем,что изначально ничья
+	for (int i = 0; i < n; i++)
+	{
+		if (field[i] == 0 || field[i] == ' ')
+		{
+			draw = false;
+			break;
+		}
+	}
+	if (draw)
+	{
+		cout << "Ничья" << endl;
+		return;
+	}
+    /*if (game_over == 9)
+	{
+		cout<< "Победила Дружба!\n";
+		return ;
+	}*/
+	//if (i == 10)
+		//	cout<<player<< "Победила Дружба!\n";
+		//		return;
+
+	//for (int i = 0; i <=10; i++)  //сразу выодит ничью
+	//{   //if ( i ==10)
+	//	cout<<player<< "Победила Дружба!\n";
+	//		return;
+	//}
+	/*for(int i=0;i<9;i++)
+	{
+		if (field[i] == 10)
+			cout << player << "Победила Дружба!\n";
+		return;
+	}*/
 	//PrintField(field, n, player == 'X' ? '0' : 'X');
 	Move(field, n, player == 'X' ? '0' : 'X');
 }
